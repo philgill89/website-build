4f5b886d4aceddff7433910f82382605d0202102
philgill89 <phil.gillett@techquity.co.uk>
2023-08-04 13:51:35 +0100

    sorts out element naming

--- index.php
+++ index.php
@@ -11,12 +11,39 @@
 <body>
     <section>
       <div class="hero">
-          <div class="hero-text">
-        <p class="age">An Age to make</p>
-          <h1 class="lifetime">A Lifetime to Love</h1>
-          <p class="beautiful">We create beautiful products from the finest sheepskin and leather. Designed to<br>last years, or even decades, to come</p>
-          <button class="sheepskin">Women's Sheepskins</button>
+          <div class="hero__text">
+              <p class="hero__subtitle">An Age to make</p>
+          <h1 class="hero__title">A Lifetime to Love</h1>
+          <p class="hero__body">We create beautiful products from the finest sheepskin and leather. Designed to last years, or even decades, to come</p>
+          <button class="button button--primary">Women's Sheepskins</button>
       </div>
       </div>
+    </section>
+
+    <section class="promos">
+        <div class="promo">
+            <img class="promo__image" src="/assets/Promo1.jpg">
+            <div class="promo__content">
+                <h5 class="promo__title">Promo 1</h5>
+                <p class="promo__body">Welcome to my first website, here you’ll find components and sections that i’ve built.</p>
+                <button class="button button--secondary">find out more</button>
+            </div>
+        </div>
+        <div class="promo">
+            <img class="promo__image" src="/assets/Promo3.jpg">
+            <div class="promo__content">
+                <h5 class="promo__title">Promo 1</h5>
+                <p class="promo__body">Welcome to my first website, here you’ll find components and sections that i’ve built.</p>
+                <button class="button button--secondary">find out more</button>
+            </div>
+        </div>
+        <div class="promo">
+            <img class="promo__image" src="/assets/Promo2.jpg">
+            <div class="promo__content">
+                <h5 class="promo__title">Mens Shirts</h5>
+                <p class="promo__body">Welcome to my first website, here you’ll find components and sections that i’ve built.</p>
+                <button class="button button--secondary">find out more</button>
+            </div>
+        </div>
     </section>
 </body>

253b84eced2c2164bab5548a6e43a950e9c02cd5
philgill89 <phil.gillett@techquity.co.uk>
2023-09-22 13:17:23 +0100

    promo spacing

--- promo-card.php
+++ promo-card.php
@@ -1,5 +1,5 @@
 <div class="promo">
-    <img class="promo__image" src="<?php echo $promo['image']; ?>">
+    <img class="promo__image"  src="<?php echo $promo['image']; ?>">
     <div class="promo__content">
         <h5 class="promo__title"><?php echo $promo['title']; ?></h5>
         <p class="promo__body"><?php echo $promo['body']; ?></p>

c620f822e7a750aca32bc22b9043a39867323747
philgill89 <phil.gillett@techquity.co.uk>
2023-10-02 15:24:33 +0100

    button styling

--- index.php
+++ index.php
@@ -4,4 +4,8 @@
     <?php includeWithVariables('elements/button.php', array('text' => 'Click Here', 'style' => 'button--primary')); ?>
     <?php includeWithVariables('elements/button.php', array('text' => 'Click Here', 'style' => 'button--secondary', 'icon' => 'caret-circle-right' )); ?>
     <?php includeWithVariables('elements/button.php', array('style' => 'button--secondary', 'icon' => 'caret-circle-right' )); ?>
+
+
 </body>
+
+

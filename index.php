fcdb85b61434ae309e6424a0362a91bfe189c865
philgill89 <phil.gillett@techquity.co.uk>
2023-10-05 15:54:31 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php include('head.php');?>
 
 <body>
-    <?php includeWithVariables('elements/button.php', array('text' => 'Click Here', 'style' => 'button--primary')); ?>
-    <?php includeWithVariables('elements/button.php', array('text' => 'Click Here', 'style' => 'button--secondary', 'icon' => 'caret-circle-right' )); ?>
-    <?php includeWithVariables('elements/button.php', array('style' => 'button--secondary', 'icon' => 'caret-circle-right' )); ?>
+
+<footer>
 
 
+</footer>
 </body>
 
 

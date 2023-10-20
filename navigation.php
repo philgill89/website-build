f79ed9559a481a7076bb136384de7b8748da7c2c
philgill89 <phil.gillett@techquity.co.uk>
2023-10-20 11:41:10 +0100

    creating nav links

---
+++ navigation.php
@@ -0,0 +1,41 @@
+<?php include('head.php'); ?>
+<div id="navigation">
+    <body class="min-h-screen flex flex-col">
+        <header class="mt-auto bg-white w-full flex pt-20 pb-10">
+            <div class="w-full max-w-screen-xl mx-auto flex flex-grid grid-cols-3">
+                    <h5 class="font-bold text-blue-950">Popular</h5>
+                        <ul class="list-none">
+                            <li><a href="#">Yorkshire Terrier</a></li>
+                            <li><a href="#">Pug</a></li>
+                            <li><a href="#">Cocker Spaniel</a></li>
+                            <li><a href="#">Jack Russel Terrier</a></li>
+                            <li><a href="#">Beagle</a></li>
+                            <li><a href="#">Husky</a></li>
+                            <li><a href="#">Samoyed</a></li>
+                            <li><a href="#">Labrador</a></li>
+                            <li><a href="#">Tosa</a></li>
+                            <li><a href="#">Dachshund</a></li>
+                            <li><a href="#">French Bulldog</a></li>
+                            <li><a href="#">Chihuahua</a></li>
+                        </ul>
+                    <h5 class="font-bold text-blue-950">Large Dog</h5>
+                        <ul class="list-none>
+                            <li><a href="#">Akita</a></li>
+                            <li><a href="#">Alaskan Malamute</a></li>
+                            <li><a href="#">Bernese Mountain</a></li>
+                            <li><a href="#">Bloodhound</a></li>
+                            <li><a href="#">Bulmastiff</a></li>
+                            <li><a href="#">Cane Corso</a></li>
+                        </ul>
+                    <h5 class="font-bold text-blue-950">Small Dog</h5>
+                        <ul class="list-none">
+                            <li><a href="#">Beagle</a></li>
+                            <li><a href="#">Boston Terrier</a></li>
+                            <li><a href="#">Calm Terrier</a></li>
+                            <li><a href="#">Chihuahua</a></li>
+                            <li><a href="#">Cocker Spaniel</a></li>
+                            <li><a href="#">Dachshund</a></li>
+                        </ul>
+        </div>
+    </header>
+</body>

fc9a9b96041c9346aef5ae5607167fa7997f86f7
philgill89 <phil.gillett@techquity.co.uk>
2023-10-20 13:52:11 +0100

    updates nav

--- navigation.php
+++ navigation.php
@@ -1,9 +1,13 @@
 <?php include('head.php'); ?>
 <div id="navigation">
     <body class="min-h-screen flex flex-col">
-        <header class="mt-auto bg-white w-full flex pt-20 pb-10">
-            <div class="w-full max-w-screen-xl mx-auto flex flex-grid grid-cols-3">
+        <header class="mt-auto bg-white w-full border border-solid flex pt-24 pb-12">
+            <div class="w-full max-w-screen-xl mx-auto grid grid-cols-3 gap-y-8">
+                <div class="flex">
                     <h5 class="font-bold text-blue-950">Popular</h5>
+                            <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
+                                <path d="M5.54169 9.99992H13.4584M13.4584 9.99992L10.2917 6.83325M13.4584 9.99992L10.2917 13.1666" stroke="#002A48" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
                         <ul class="list-none">
                             <li><a href="#">Yorkshire Terrier</a></li>
                             <li><a href="#">Pug</a></li>
@@ -19,6 +23,9 @@
                             <li><a href="#">Chihuahua</a></li>
                         </ul>
                     <h5 class="font-bold text-blue-950">Large Dog</h5>
+                            <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
+                                <path d="M5.54169 9.99992H13.4584M13.4584 9.99992L10.2917 6.83325M13.4584 9.99992L10.2917 13.1666" stroke="#002A48" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
                         <ul class="list-none>
                             <li><a href="#">Akita</a></li>
                             <li><a href="#">Alaskan Malamute</a></li>
@@ -28,6 +35,9 @@
                             <li><a href="#">Cane Corso</a></li>
                         </ul>
                     <h5 class="font-bold text-blue-950">Small Dog</h5>
+                            <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
+                                <path d="M5.54169 9.99992H13.4584M13.4584 9.99992L10.2917 6.83325M13.4584 9.99992L10.2917 13.1666" stroke="#002A48" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
                         <ul class="list-none">
                             <li><a href="#">Beagle</a></li>
                             <li><a href="#">Boston Terrier</a></li>
@@ -36,6 +46,8 @@
                             <li><a href="#">Cocker Spaniel</a></li>
                             <li><a href="#">Dachshund</a></li>
                         </ul>
+                </div>
+                <img src="assets/adopt.jpg">
         </div>
     </header>
 </body>

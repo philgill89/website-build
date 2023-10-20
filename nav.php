025b734ec38785d33c4685b5df0b5bdace07f206
cbendelow <41581358+cbendelow@users.noreply.github.com>
2023-10-20 16:57:32 +0100

    nav

---
+++ nav.php
@@ -0,0 +1,70 @@
+<?php
+?>
+<div class="hidden group-hover:block w-full absolute top-full left-0 pt-8">
+    <div class="w-full bg-white rounded-2xl shadow-2xl flex p-16">
+        <div class="w-full max-w-screen-xl flex mx-auto gap-y-8">
+            <?php /*foreach($navs as $nav){
+
+            }*/ ?>
+
+            <div class="space-y-4 mr-12">
+                <a class="text-xl flex items-center space-x-4" href="">
+                    <span class="font-bold text-blue-950">Popular</span>
+                    <svg class="h-6 w-6 fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="12" fill="none" viewBox="0 0 20 12">
+                        <path d="M13.293.293a1 1 0 0 1 1.414 0l5 5a1 1 0 0 1 0 1.414l-5 5a1 1 0 0 1-1.414-1.414L16.586 7H1a1 1 0 0 1 0-2h15.586l-3.293-3.293a1 1 0 0 1 0-1.414Z"
+                              clip-rule="evenodd"/>
+                    </svg>
+                </a>
+                <ul class="columns-2 gap-8 font-normal text-black">
+                    <li><a class="hover:underline" href="#">Yorkshire Terrier</a></li>
+                    <li><a class="hover:underline" href="#">Pug</a></li>
+                    <li><a class="hover:underline" href="#">Cocker Spaniel</a></li>
+                    <li><a class="hover:underline" href="#">Jack Russel Terrier</a></li>
+                    <li><a class="hover:underline" href="#">Beagle</a></li>
+                    <li><a class="hover:underline" href="#">Husky</a></li>
+                    <li><a class="hover:underline" href="#">Samoyed</a></li>
+                    <li><a class="hover:underline" href="#">Labrador</a></li>
+                    <li><a class="hover:underline" href="#">Tosa</a></li>
+                    <li><a class="hover:underline" href="#">Dachshund</a></li>
+                    <li><a class="hover:underline" href="#">French Bulldog</a></li>
+                    <li><a class="hover:underline" href="#">Chihuahua</a></li>
+                </ul>
+            </div>
+            <div class="space-y-4 mr-12">
+                <a class="text-xl flex items-center space-x-4" href="">
+                    <span class="font-bold text-blue-950">Popular</span>
+                    <svg class="h-6 w-6 fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="12" fill="none" viewBox="0 0 20 12">
+                        <path d="M13.293.293a1 1 0 0 1 1.414 0l5 5a1 1 0 0 1 0 1.414l-5 5a1 1 0 0 1-1.414-1.414L16.586 7H1a1 1 0 0 1 0-2h15.586l-3.293-3.293a1 1 0 0 1 0-1.414Z"
+                              clip-rule="evenodd"/>
+                    </svg>
+                </a>
+                <ul class="font-normal text-black">
+                    <li><a class="hover:underline" href="#">Akita</a></li>
+                    <li><a class="hover:underline" href="#">Alaskan Malamute</a></li>
+                    <li><a class="hover:underline" href="#">Bernese Mountain</a></li>
+                    <li><a class="hover:underline" href="#">Bloodhound</a></li>
+                    <li><a class="hover:underline" href="#">Bulmastiff</a></li>
+                    <li><a class="hover:underline" href="#">Cane Corso</a></li>
+                </ul>
+            </div>
+            <div class="space-y-4">
+                <a class="text-xl flex items-center space-x-4" href="">
+                    <span class="font-bold text-blue-950">Popular</span>
+                    <svg class="h-6 w-6 fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="12" fill="none" viewBox="0 0 20 12">
+                        <path d="M13.293.293a1 1 0 0 1 1.414 0l5 5a1 1 0 0 1 0 1.414l-5 5a1 1 0 0 1-1.414-1.414L16.586 7H1a1 1 0 0 1 0-2h15.586l-3.293-3.293a1 1 0 0 1 0-1.414Z"
+                              clip-rule="evenodd"/>
+                    </svg>
+                </a>
+                <ul class="font-normal text-black">
+                    <li><a class="hover:underline" href="#">Beagle</a></li>
+                    <li><a class="hover:underline" href="#">Boston Terrier</a></li>
+                    <li><a class="hover:underline" href="#">Calm Terrier</a></li>
+                    <li><a class="hover:underline" href="#">Chihuahua</a></li>
+                    <li><a class="hover:underline" href="#">Cocker Spaniel</a></li>
+                    <li><a class="hover:underline" href="#">Dachshund</a></li>
+                </ul>
+            </div>
+            <img class="ml-auto" src="assets/adopt.jpg">
+        </div>
+    </div>
+</div>
--- navigation.php
+++ navigation.php
@@ -1,53 +1,52 @@
-<?php include('head.php'); ?>
-<div id="navigation">
-    <body class="min-h-screen flex flex-col">
-        <header class="mt-auto bg-white w-full border border-solid flex pt-24 pb-12">
-            <div class="w-full max-w-screen-xl mx-auto grid grid-cols-3 gap-y-8">
-                <div class="flex">
-                    <h5 class="font-bold text-blue-950">Popular</h5>
-                            <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
-                                <path d="M5.54169 9.99992H13.4584M13.4584 9.99992L10.2917 6.83325M13.4584 9.99992L10.2917 13.1666" stroke="#002A48" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                            </svg>
-                        <ul class="list-none">
-                            <li><a href="#">Yorkshire Terrier</a></li>
-                            <li><a href="#">Pug</a></li>
-                            <li><a href="#">Cocker Spaniel</a></li>
-                            <li><a href="#">Jack Russel Terrier</a></li>
-                            <li><a href="#">Beagle</a></li>
-                            <li><a href="#">Husky</a></li>
-                            <li><a href="#">Samoyed</a></li>
-                            <li><a href="#">Labrador</a></li>
-                            <li><a href="#">Tosa</a></li>
-                            <li><a href="#">Dachshund</a></li>
-                            <li><a href="#">French Bulldog</a></li>
-                            <li><a href="#">Chihuahua</a></li>
-                        </ul>
-                    <h5 class="font-bold text-blue-950">Large Dog</h5>
-                            <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
-                                <path d="M5.54169 9.99992H13.4584M13.4584 9.99992L10.2917 6.83325M13.4584 9.99992L10.2917 13.1666" stroke="#002A48" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                            </svg>
-                        <ul class="list-none>
-                            <li><a href="#">Akita</a></li>
-                            <li><a href="#">Alaskan Malamute</a></li>
-                            <li><a href="#">Bernese Mountain</a></li>
-                            <li><a href="#">Bloodhound</a></li>
-                            <li><a href="#">Bulmastiff</a></li>
-                            <li><a href="#">Cane Corso</a></li>
-                        </ul>
-                    <h5 class="font-bold text-blue-950">Small Dog</h5>
-                            <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
-                                <path d="M5.54169 9.99992H13.4584M13.4584 9.99992L10.2917 6.83325M13.4584 9.99992L10.2917 13.1666" stroke="#002A48" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                            </svg>
-                        <ul class="list-none">
-                            <li><a href="#">Beagle</a></li>
-                            <li><a href="#">Boston Terrier</a></li>
-                            <li><a href="#">Calm Terrier</a></li>
-                            <li><a href="#">Chihuahua</a></li>
-                            <li><a href="#">Cocker Spaniel</a></li>
-                            <li><a href="#">Dachshund</a></li>
-                        </ul>
-                </div>
-                <img src="assets/adopt.jpg">
+<div class="mt-auto bg-white w-full border border-solid flex pt-24 pb-12">
+    <div class="w-full max-w-screen-xl mx-auto grid grid-cols-3 gap-y-8">
+        <div class="flex">
+            <h5 class="font-bold text-blue-950">Popular</h5>
+            <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
+                <path d="M5.54169 9.99992H13.4584M13.4584 9.99992L10.2917 6.83325M13.4584 9.99992L10.2917 13.1666"
+                      stroke="#002A48" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+            <ul class="list-none">
+                <li><a href="#">Yorkshire Terrier</a></li>
+                <li><a href="#">Pug</a></li>
+                <li><a href="#">Cocker Spaniel</a></li>
+                <li><a href="#">Jack Russel Terrier</a></li>
+                <li><a href="#">Beagle</a></li>
+                <li><a href="#">Husky</a></li>
+                <li><a href="#">Samoyed</a></li>
+                <li><a href="#">Labrador</a></li>
+                <li><a href="#">Tosa</a></li>
+                <li><a href="#">Dachshund</a></li>
+                <li><a href="#">French Bulldog</a></li>
+                <li><a href="#">Chihuahua</a></li>
+            </ul>
+            <h5 class="font-bold text-blue-950">Large Dog</h5>
+            <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
+                <path d="M5.54169 9.99992H13.4584M13.4584 9.99992L10.2917 6.83325M13.4584 9.99992L10.2917 13.1666"
+                      stroke="#002A48" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+            <ul>
+                <li><a href="#">Akita</a></li>
+                <li><a href="#">Alaskan Malamute</a></li>
+                <li><a href="#">Bernese Mountain</a></li>
+                <li><a href="#">Bloodhound</a></li>
+                <li><a href="#">Bulmastiff</a></li>
+                <li><a href="#">Cane Corso</a></li>
+            </ul>
+            <h5 class="font-bold text-blue-950">Small Dog</h5>
+            <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
+                <path d="M5.54169 9.99992H13.4584M13.4584 9.99992L10.2917 6.83325M13.4584 9.99992L10.2917 13.1666"
+                      stroke="#002A48" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+            <ul class="list-none">
+                <li><a href="#">Beagle</a></li>
+                <li><a href="#">Boston Terrier</a></li>
+                <li><a href="#">Calm Terrier</a></li>
+                <li><a href="#">Chihuahua</a></li>
+                <li><a href="#">Cocker Spaniel</a></li>
+                <li><a href="#">Dachshund</a></li>
+            </ul>
         </div>
-    </header>
-</body>
+        <img src="assets/adopt.jpg">
+    </div>
+</div>

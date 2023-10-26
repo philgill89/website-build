63048a705fc76d83bebacc4d2dfa9f251b69ea75
philgill89 <phil.gillett@techquity.co.uk>
2023-10-26 15:12:35 +0100

    creating product cards

--- promo-card.php
+++ promo-card.php
@@ -1,8 +1,39 @@
-<div class="promo">
-    <img class="promo__image"  src="<?php echo $promo['image']; ?>">
-    <div class="promo__content">
-        <h5 class="promo__title"><?php echo $promo['title']; ?></h5>
-        <p class="promo__body"><?php echo $promo['body']; ?></p>
-        <button class="button button--secondary"><?php echo $promo['button']; ?></button>
+<?php include('head.php');?>
+
+<body class="m8 lg:m-16">
+<div class="w-full mb-16">
+    <div class="flex flex-wrap gap-8">
+        <div>
+            <div class="grid grid-cols-1 p-8 rounded-lg">
+                <div class="rounded-xl">
+                    <img src="assets/dog1.jpg">
+                </div>
+                <h5>MO231 - Pomeranian White</h5>
+                <span>
+                <p>Gender: Male</p>
+                <p>Age: 2 months</p>
+            </span>
+                <h5>£2000</h5>
+            </div>
+        </div>
+        <div>
+            <div class=grid grid-cols-1 items-center gap-8 p-8 rounded-lg">
+            <div class="rounded-xl">
+                <img class="rounded-xl" src="assets/food.jpg">
+            </div>
+            <h5>MO231 - Pomeranian White</h5>
+            <span>
+                <p>Product: Food</p>
+                <p>Weight: 385g</p>
+            </span>
+            <h5>£2000</h5>
+
+                <?php includeWithVariables('elements/button.php',
+                    array('text' => 'Free Toy & Free Shaker', 'style' => 'button--secondary rounded-xl bg-beige text-black')); ?>
+            </div>
+        </div>
+        <div>
+        </div>
     </div>
 </div>
+</body>

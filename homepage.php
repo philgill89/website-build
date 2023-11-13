7216deaea0c47c7b33ef6b0cc9d6bbd072e2472c
philgill89 <phil.gillett@techquity.co.uk>
2023-11-13 13:25:19 +0000

    creating the homepage

--- homepage.php
+++ homepage.php
@@ -1,27 +1,43 @@
 <?php include('head.php');?>
-<body class="min-h-screen flex flex-col mt-auto w-full">
-    <header class="relative max-w-screen-xl mx-auto">
-            <div class="w-full">
-                <img alt="happy woman and dog" class="w-full relative" src="../assets/hero.jpg">
-                <div class="absolute grid grid-cols-1">
-                    <span class="font-bold text-primary text-3xl">One more friend</span>
-                    <span class="text-primary font-bold">Thousands more fun!</span>
-                </div>
-            </div>
-    </header>
-    <!--<div>
-        <p>Whats new?</p>
-        <h5 class="text-primary font-bold">Take a look at some of our pets</h5>
-</div>-->
-    <div class="items-center">
-        <img src="assets/banner1.jpg">
+<body class="min-h-screen flex relative max-w-screen-xl mx-auto">
+<div class="mt-auto w-full rounded-t-3xl flex flex-col pt-24 pb-12">
+    <div class="flex items-center w-full">
+
+        <img alt="happy woman and dog" class="relative" src="../assets/hero.jpg">
+        <div class="absolute grid grid-cols-1 px-14">
+            <h1 class="font-bold text-primary text-4xl">One more friend</h1>
+            <h1 class="text-primary font-bold text-4xl">Thousands more fun!</h1>
+        </div>
     </div>
 
-    <div class="">
-        <div class="grid grid-cols-1>
-        <span class="">Proud to be part of</span>
+    <div class="grid grid-cols-1">
+        <span class="text-primary">Whats new?</span>
+        <span class="text-primary">Take a look at some of our pets</span>
+    </div>
+        <div class="grid grid col-span-4">
+        </div>
+
+    <div class="flex">
+        <img src="assets/banner1.jpg" class="relative">
+        <div class="flex absolute grid grid-cols-1 justify-items-end">
+            <h3 class="font-bold text-primary text-4xl">One more friend</h3>
+            <h3 class="text-primary font-bold text-4xl">Thousands more fun!</h3>
+        </div>
+    </div>
+
+    <div class="grid grid-cols-1">
+        <span class="text-primary">Hard to choose the right products for your pets?</span>
+        <span class="text-primary">Our Products</span>
+    </div>
+    <div class="grid grid col-span-4">
+    </div>
+
+
+        <div class="grid grid-cols-2>
+            <span class="">Proud to be part of</span>
             <span class="text-primary font-bold">Pet Sellers</span>
-        <div class="grid grid-cols-7 items-center flex flex-wrap">
+        </div>
+        <div class="grid grid-cols-7 items-center justify-between">
             <img src="assets/logo1.jpg">
             <img src="assets/logo2.jpg">
             <img src="assets/logo3.jpg">
@@ -30,13 +46,23 @@
             <img src="assets/logo6.jpg">
             <img src="assets/logo7.jpg">
         </div>
+
+<div class="grid grid-cols-1">
+    <span class="text-primary">You already know?</span>
+    <span class="text-primary">Useful Pet Knowledge</span>
+</div>
+<div class="grid grid col-span-4">
+</div>
+
+    <div class="items-center justify-center flex">
+        <img src="assets/banner2.jpg">
+        <div class="absolute grid grid-cols-1">
+            <h3 class="font-bold text-primary text-4xl">Adoption</h3>
+            <h3 class="font-bold text-primary text-4xl">We need help. So do they</h3>
+            <Span class="text-secondary ">Adopt a pet and give it a home, it will love you back unconditionally</Span>
         </div>
     </div>
-
-    <div class="items-center">
-        <img src="assets/banner2.jpg">
-    </div>
-    <footer class="mt-auto bg-beige w-full rounded-t-3xl flex pt-24 pb-12">
+    <footer class="bg-beige w-full rounded-t-3xl flex pt-24 pb-12">
         <div class="w-full max-w-screen-xl mx-auto grid grid-cols-1 gap-y-12">
             <div class="bg-primary rounded-xl p-6">
                 <div class="flex items-center">

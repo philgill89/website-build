b1535849e835688d59d687bcbd6293ef638abd10
philgill89 <phil.gillett@techquity.co.uk>
2023-12-05 16:52:47 +0000

    Update listing-cards.php

--- listing-cards.php
+++ listing-cards.php
@@ -8,38 +8,111 @@
             $products = array(
                 "dog-1" => array(
                     "title" => "MO231 - Pomeranian White",
+                    "Gender" => "Male",
+                    "Age" => "02 months",
                     "price" => "£50.00",
                 ),
                 "dog-2" => array(
                     "title" => "MO231 - Poodle Tiny Yellow",
+                    "Gender" => "Female",
+                    "Age" => "02 months",
                     "price" => "£50.00",
                 ),
                 "dog-3" => array(
                     "title" => "M0102 - Poodle Tiny Sepia",
+                    "Gender" => "Male",
+                    "Age" => "02 months",
                     "price" => "£50.00",
                 ),
                  "dog-4" => array(
                     "title" => "M0512 - Alaskan Malamute Grey",
+                    "Gender" => "Male",
+                    "Age" => "02 months",
                     "price" => "£50.00",
                 ),
                 "dog-5" => array(
                     "title" => "MO231 - Pembroke Corgi Cream",
+                    "Gender" => "Male",
+                    "Age" => "02 months",
                     "price" => "£50.00",
                 ),
                 "dog-6" => array(
                     "title" => "MO231 - Pembroke Corgi Tricolour",
+                    "Gender" => "Female",
+                    "Age" => "02 months",
                     "price" => "£50.00",
                 ),
                 "dog-7" => array(
                     "title" => "M0231 - Pomeranian White",
+                    "Gender" => "Female",
+                    "Age" => "02 months",
                     "price" => "£50.00",
                 ),
                 "dog-8" => array(
                     "title" => "M0312 - Poodle Tiny Dairy Cow",
+                    "Gender" => "Male",
+                    "Age" => "02 months",
                     "price" => "£50.00",
                 ),
             );
         ?>
+
+        <?php
+        $products = array(
+            "product-1" => array(
+                "title" => "Reflex Plus Adult Cat Food",
+                "Product" => "Cat Food",
+                "Size" => "385g",
+                "price" => "£50.00",
+            ),
+            "product-2" => array(
+                "title" => "Reflex Plus Adult Cat Food Salmon",
+                "Product" => "Cat Food",
+                "Size" => "1.5kg",
+                "price" => "£50.00",
+            ),
+            "product-3" => array(
+                "title" => "Cat scratching ball toy kitten slsal rope ball",
+                "Product" => "Toy",
+                "price" => "£50.00",
+            ),
+            "product-4" => array(
+                "title" => "Cute Pet Cat Warm Nest",
+                "Product" => "Toy",
+                "price" => "£50.00",
+            ),
+            "product-5" => array(
+                "title" => "NaturVet Dogs - Omega-Gold Plus Salmon Oil",
+                "Product" => "Dog Food",
+                "Size" => "385g",
+                "price" => "£50.00",
+            ),
+            "product-6" => array(
+                "title" => "Costume Fashion Pet Clother Cowboy Rider",
+                "Product" => "Costume",
+                "Size" => "Small",
+                "price" => "£50.00",
+            ),
+            "product-7" => array(
+                "title" => "M0231 - Pomeranian White",
+                "Product" => "Costume",
+                "Size" => "Medium",
+                "price" => "£50.00",
+            ),
+            "product-8" => array(
+                "title" => "Plush Pet Toy",
+                "product" => "Toy",
+                "price" => "£50.00",
+            ),
+        );
+        ?>
+
+
+
+
+
+
+
 
         <?php foreach($products as $product){
             includeWithVariables('/listing-card.php',
@@ -53,35 +126,6 @@
         <?php includeWithVariables('/listing-card.php',
             array('title' => 'test')); ?>
 
-
-       <!-- <div>
-            <div class="grid grid-cols-1 gap-y-2 rounded-lg p-4 border border-solid">
-                <div class="rounded-xl">
-                    <img class="rounded-xl" src="assets/dog1.jpg">
-                </div>
-                <h5 class="font-bold">MO231 - Pomeranian White</h5>
-                <span class="grid grid-cols-2">
-                <p>Gender: Male</p>
-                <p>Age: 2 months</p>
-            </span>
-                <h5 class="font-bold">£2000</h5>
-            </div>
-        </div>
-        <div>
-            <div class="grid grid-cols-1 gap-y-2 rounded-lg p-4 border border-solid">
-            <div class="rounded-xl">
-                <img class="rounded-xl" src="assets/food.jpg">
-            </div>
-            <h5 class="font-bold">MO231 - Pomeranian White</h5>
-            <span class="grid grid-cols-2">
-                <p>Product: Food</p>
-                <p>Weight: 385g</p>
-            </span>
-            <h5 class="font-bold">£2000</h5>
-            </div>
-        </div>
-        <div>
-        </div> -->
     </div>
     <?php if(isset($title)){echo $title;} ?>
     <?php if(isset($price)){echo $price;} ?>
